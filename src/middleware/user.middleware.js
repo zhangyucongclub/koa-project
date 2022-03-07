6cb989fd7fe4d45e15f5cf680ecf563fa7b54cbd
zhangyucong02 <zhangyucong02@58.com>
2022-03-07 23:01:18 +0800

    feat:加密

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,5 +1,9 @@
+const bcrypt = require("bcryptjs")
 const { getUserInfo } = require("../service/user.service")
-const { userFormateError, userAlreadyExited } = require('../constant/err.type')
+const { userFormateError, userAlreadyExited,userRegisterError,
+    userDoesNotExist,
+    userLoginError,
+    invalidPassword, } = require('../constant/err.type')
 const userValidator = async (ctx, next) => {
     const { user_name, password } = ctx.request.body
     // 合法性
@@ -13,10 +17,55 @@ const userValidator = async (ctx, next) => {
 }
 const verifyUser = async (ctx, next) => {
     const { user_name } = ctx.request.body
+    try {
+        const res = await getUserInfo({ user_name })
+    
+        if (res) {
+          console.error('用户名已经存在', { user_name })
+          ctx.app.emit('error', userAlreadyExited, ctx)
+          return
+        }
+    } catch (err) {
+        console.error('获取用户信息错误', err)
+        ctx.app.emit('error', userRegisterError, ctx)
+        return
+    }
   
-    if (await getUserInfo({ user_name })) {
-      ctx.app.emit('error', userAlreadyExited, ctx)
-      return
+    await next()
+}
+// 加密
+const crpytPassword = async (ctx, next) => {
+    const { password } = ctx.request.body
+  
+    const salt = bcrypt.genSaltSync(10)
+    // hash保存的是 密文
+    const hash = bcrypt.hashSync(password, salt)
+  
+    ctx.request.body.password = hash
+  
+    await next()
+  }
+const verifyLogin = async (ctx, next) => {
+    // 1. 判断用户是否存在(不存在:报错)
+    const { user_name, password } = ctx.request.body
+  
+    try {
+      const res = await getUserInfo({ user_name })
+  
+      if (!res) {
+        console.error('用户名不存在', { user_name })
+        ctx.app.emit('error', userDoesNotExist, ctx)
+        return
+      }
+  
+      // 2. 密码是否匹配(不匹配: 报错)
+      if (!bcrypt.compareSync(password, res.password)) {
+        ctx.app.emit('error', invalidPassword, ctx)
+        return
+      }
+    } catch (err) {
+      console.error(err)
+      return ctx.app.emit('error', userLoginError, ctx)
     }
   
     await next()
@@ -24,4 +73,6 @@ const verifyUser = async (ctx, next) => {
 module.exports = {
     userValidator,
     verifyUser,
+    crpytPassword,
+    verifyLogin
 }
