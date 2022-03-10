0325c52a337e234a522387f7a7910cd7031ad225
zhangyucong02 <zhangyucong02@58.com>
2022-03-10 22:39:22 +0800

    feat:用户模块

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,6 @@
-const { createUser } = require('../service/user.service')
+const jwt = require('jsonwebtoken')
+const { createUser,getUserInfo,updateById } = require('../service/user.service')
+const { JWT_SECRET } = require("../config/config.default")
 class UserController {
     async register(ctx,next){
         // 获取数据
@@ -17,7 +19,38 @@ class UserController {
         }
     }
     async login(ctx,next){
-        ctx.body="登录成功"
+        const { user_name } = ctx.request.body
+        try {
+            // 从返回结果对象中剔除password属性, 将剩下的属性放到res对象
+            const { password, ...res } = await getUserInfo({ user_name })
+        
+            ctx.body = {
+              code: 0,
+              message: '用户登录成功',
+              result: {
+                token: jwt.sign(res, JWT_SECRET, { expiresIn: '1d' }),
+              },
+            }
+          } catch (err) {
+            console.error('用户登录失败', err)
+        }
+    }
+    async changePassword(ctx,next){
+        const id =ctx.state.user.id
+        const password = ctx.request.body.password
+        if(await updateById({id,password})){
+            ctx.body = {
+                code:'0',
+                message:'修改密码成功！',
+                result:''
+            }
+        }else{
+            ctx.body = {
+                code:'10007',
+                message:'修改密码失败！',
+                result:''
+            }    
+        }
     }
 }
 
--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,6 +1,7 @@
 const Router = require('koa-router');
 const { userValidator,verifyUser,crpytPassword,verifyLogin }=require("../middleware/user.middleware")
-const { register,login } = require('../controller/user.controller');
+const { auth } = require("../middleware/auth.middleware")
+const { register,login,changePassword } = require('../controller/user.controller');
 const router = new Router({prefix:'/users'});
 
 // 注册接口
@@ -8,5 +9,8 @@ router.post('/register',userValidator,verifyUser,crpytPassword,register)
 
 //登录接口
 router.post('/login',userValidator,verifyLogin,login)
+
+//修改密码
+router.patch('/',auth,crpytPassword,changePassword)
 
 module.exports = router
